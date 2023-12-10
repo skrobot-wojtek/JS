f44e17cd6502be3ff5afee4d6d40d31ed0e6cf8a
Wojtek Skrobot <97315300+skrobot-wojtek@users.noreply.github.com>
2023-12-10 23:19:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// const btn = document.querySelector("button");
-let number =1;
+let number = 1;   
+let ul1 = document.querySelector("ul");
 const addElement = function () {
-const div = document.createElement("div");
-div.textContent = number;
-if(number%5==0){
-    div.classList.add("cirlce")
-}
-    document.body.appendChild(div);
-    number++;
+
+    const li = document.createElement("li");
+    li.textContent = number;
+    if (number % 3 === 0) {
+        li.classList.add("podzielne")
+    }
+   ul1.appendChild(li);
+    number += 2;
 }
 
 // btn.addEventListener("click", addElement);
